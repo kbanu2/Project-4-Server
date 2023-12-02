6cf17fd4b5ab92026524119ff1ac858c696d113f
kbanushi <krenarban@gmail.com>
2023-12-02 16:15:09 -0600

    Added MinMax algorithm using a string to represent the game board

---
+++ src/main/java/ServerGUI.java
@@ -0,0 +1,70 @@
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class ServerGUI extends Application {
+    private TextField portTextField = new TextField();
+    private Button createServer = new Button("Create Server");
+    private ListView<String> serverLogs = new ListView<>();
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Create Server");
+        primaryStage.setScene(createSetUpScene());
+
+        AI_MinMax aiMinMax = new AI_MinMax("b b X X b b O O b         ");
+
+        createServer.setOnAction(event -> {
+            primaryStage.setTitle("Server Logs");
+
+            try{
+                //ToDo: Implement code to create server thread with input port
+            }
+            catch (Exception e){
+                System.out.println("Could not start server");
+                e.printStackTrace();
+            }
+
+            primaryStage.setScene(createServerScene());
+        });
+
+        primaryStage.show();
+    }
+
+    public Scene createSetUpScene(){
+        HBox hBox1 = new HBox(new Label("Enter Server Port: "), portTextField);
+        HBox hBox2 = new HBox(new Label("Create Server: " ), createServer);
+        VBox vBox = new VBox(new Label("Create New Server"), hBox1, hBox2);
+
+        hBox1.setAlignment(Pos.CENTER);
+        hBox1.setSpacing(10);
+        hBox2.setAlignment(Pos.CENTER);
+        hBox2.setSpacing(10);
+        vBox.setAlignment(Pos.CENTER);
+        vBox.setSpacing(10);
+        return new Scene(vBox, 500, 500);
+    }
+
+    public Scene createServerScene(){
+        HBox hBox = new HBox(serverLogs);
+        VBox vBox = new VBox(hBox);
+
+        vBox.setAlignment(Pos.CENTER);
+        hBox.setAlignment(Pos.CENTER);
+        vBox.setSpacing(5);
+        hBox.setSpacing(5);
+        serverLogs.setPrefHeight(500);
+        return new Scene(vBox, 750, 750);
+    }
+}
